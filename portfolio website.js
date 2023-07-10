afd6d6ed18ecff08f34dc6b10c82ab9f2f9e2f10
Saadansari234 <saadansari3212@gmail.com>
2023-07-11 01:24:01 +0530

    this my final portfolio website

---
+++ portfolio website.js
@@ -0,0 +1,16 @@
+
+
+// // Functionality 1: Change background color on click
+// dotElement.addEventListener('click', () => {
+//   dotElement.style.backgroundColor = 'blue';
+// });
+
+// // Functionality 2: Move the dot on mouseover
+// dotElement.addEventListener('mouseover', () => {
+//   dotElement.style.transform = 'translateX(100px)';
+// });
+
+// // Functionality 3: Reset the dot to its original position on mouseout
+// dotElement.addEventListener('mouseout', () => {
+//   dotElement.style.transform = 'translateX(0)';
+// });
